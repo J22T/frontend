b01020ac477e912c3621524f966b33007586c140
J22T <jordan22taylor@gmail.com>
2024-02-23 13:23:00 -0800

    Props; Function Components

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./Card.css";
 
-type Props = {}
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card: React.FC<Props> = ({ companyName, ticker, price }: Props) : JSX.Element => {
   return (
     <div className="card">
         <img
@@ -11,8 +15,10 @@ const Card = (props: Props) => {
         alt="BTC"
         />
         <div className="details">
-            <h2>BTC</h2>
-            <p>$50,000</p>
+            <h2>
+              {companyName} ({ticker})
+            </h2>
+            <p>${price}</p>
         </div>
         <p className="info">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis consequatur alias mollitia impedit velit dignissimos est optio modi expedita enim, quibusdam temporibus quasi, explicabo illo tempora doloribus, sequi aperiam amet.</p>
      </div>
--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Card from '../Card/Card';
+import { JsxElement } from 'typescript';
 
-type Props = {}
+interface Props {};
 
-const CardList = (props: Props) => {
+const CardList : React.FC<Props>  = (props: Props): JSX.Element => {
   return (
-    <div>CardList</div>
-  )
-}
+    <div>
+      <Card companyName="BTC" ticker="BTC" price={50000} />
+      <Card companyName="Microsoft" ticker="MSFT" price={300} />
+      <Card companyName="Tesla" ticker="TSLA" price={400} />
+    </div>
+  );
+};
 
-export default CardList
+export default CardList;
